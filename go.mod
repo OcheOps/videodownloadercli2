399bff0afc16d700408dc2afb32108d937db64e7
Oche <81412791+OcheOps@users.noreply.github.com>
2024-07-14 00:10:32 +0100

    it's working

--- go.mod
+++ go.mod
@@ -2,8 +2,16 @@ module github.com/OcheOps/videodownloadercli2
 
 go 1.22.5
 
+require github.com/kkdai/youtube/v2 v2.10.1
+
 require (
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/dlclark/regexp2 v1.11.0 // indirect
+	github.com/dop251/goja v0.0.0-20240220182346-e401ed450204 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/google/pprof v0.0.0-20240227163752-401108e1b7e7 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
+	neturl "net/url"
 	"os"
 	"path/filepath"
-	neturl "net/url"
+	"sort"
+	"strings"
+	"encoding/json"
+    "net/http"
+    
+    "regexp"
+    "time"
 
-	"github.com/spf13/cobra"
 	"github.com/kkdai/youtube/v2"
+	"github.com/spf13/cobra"
 )
 
 func main() {
@@ -94,11 +102,179 @@ func validateURL(url string) (string, error) {
 }
 
 func downloadYouTubeVideo(url, downloadDir string) error {
-	// TODO: Implement YouTube video downloading
-	return fmt.Errorf("YouTube video downloading not implemented yet")
+    client := youtube.Client{}
+
+    video, err := client.GetVideo(url)
+    if err != nil {
+        return fmt.Errorf("error getting video info: %w", err)
+    }
+
+    formats := video.Formats.WithAudioChannels()
+    sort.Slice(formats, func(i, j int) bool {
+        return formats[i].Quality > formats[j].Quality
+    })
+
+    var format *youtube.Format
+    for _, f := range formats {
+        if f.Quality == "hd720" || f.Quality == "medium" {
+            format = &f
+            break
+        }
+    }
+
+    if format == nil {
+        return fmt.Errorf("no suitable video format found")
+    }
+
+    stream, size, err := client.GetStream(video, format)
+    if err != nil {
+        return fmt.Errorf("error getting video stream: %w", err)
+    }
+    defer stream.Close()
+
+    fileName := sanitizeFileName(video.Title) + ".mp4"
+    filePath := filepath.Join(downloadDir, fileName)
+
+    file, err := os.Create(filePath)
+    if err != nil {
+        return fmt.Errorf("error creating file: %w", err)
+    }
+    defer file.Close()
+
+    fmt.Printf("Downloading: %s\n", video.Title)
+    fmt.Printf("Size: %.2f MB\n", float64(size)/(1024*1024))
+
+    _, err = io.Copy(file, io.TeeReader(stream, &ProgressWriter{Total: size}))
+    if err != nil {
+        return fmt.Errorf("error downloading video: %w", err)
+    }
+
+    fmt.Printf("\nVideo downloaded: %s\n", filePath)
+    return nil
+}
+
+func sanitizeFileName(fileName string) string {
+    fileName = strings.Map(func(r rune) rune {
+        if r == '/' || r == '\\' || r == ':' || r == '*' || r == '?' || r == '"' || r == '<' || r == '>' || r == '|' {
+            return -1
+        }
+        return r
+    }, fileName)
+
+    fileName = strings.TrimSpace(fileName)
+
+    if len(fileName) > 200 {
+        fileName = fileName[:200]
+    }
+
+    return fileName
+}
+
+type ProgressWriter struct {
+	Total int64
+	Current int64
+}
+
+func (pw *ProgressWriter) Write(p []byte) (int, error) {
+	n := len(p)
+	pw.Current += int64(n)
+	pw.PrintProgress()
+	return n, nil
+}
+
+func (pw *ProgressWriter) PrintProgress() {
+	if pw.Total == 0 {
+		return
+	}
+	percentage := float64(pw.Current) / float64(pw.Total) * 100
+	fmt.Printf("\rProgress: %.2f%%", percentage)
 }
 
 func downloadInstagramVideo(url, downloadDir string) error {
-	// TODO: Implement Instagram video downloading
-	return fmt.Errorf("Instagram video downloading not implemented yet")
+    client := &http.Client{}
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return fmt.Errorf("error creating request: %w", err)
+    }
+
+    req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+    resp, err := client.Do(req)
+    if err != nil {
+        return fmt.Errorf("error fetching Instagram page: %w", err)
+    }
+    defer resp.Body.Close()
+
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        return fmt.Errorf("error reading Instagram page: %w", err)
+    }
+
+    re := regexp.MustCompile(`<script type="text/javascript">window\._sharedData = (.+);</script>`)
+    matches := re.FindSubmatch(body)
+    if len(matches) < 2 {
+        return fmt.Errorf("could not find shared data in Instagram page")
+    }
+
+    var data map[string]interface{}
+    err = json.Unmarshal(matches[1], &data)
+    if err != nil {
+        return fmt.Errorf("error parsing JSON data: %w", err)
+    }
+
+    entryData, ok := data["entry_data"].(map[string]interface{})
+    if !ok {
+        return fmt.Errorf("could not find entry_data in JSON")
+    }
+
+    postPage, ok := entryData["PostPage"].([]interface{})
+    if !ok || len(postPage) == 0 {
+        return fmt.Errorf("could not find PostPage in JSON")
+    }
+
+    post, ok := postPage[0].(map[string]interface{})
+    if !ok {
+        return fmt.Errorf("could not find post data in JSON")
+    }
+
+    graphql, ok := post["graphql"].(map[string]interface{})
+    if !ok {
+        return fmt.Errorf("could not find graphql data in JSON")
+    }
+
+    shortcodeMedia, ok := graphql["shortcode_media"].(map[string]interface{})
+    if !ok {
+        return fmt.Errorf("could not find shortcode_media in JSON")
+    }
+
+    videoUrl, ok := shortcodeMedia["video_url"].(string)
+    if !ok {
+        return fmt.Errorf("could not find video_url in JSON")
+    }
+
+    videoResp, err := http.Get(videoUrl)
+    if err != nil {
+        return fmt.Errorf("error downloading video: %w", err)
+    }
+    defer videoResp.Body.Close()
+
+    fileName := fmt.Sprintf("instagram_video_%d.mp4", time.Now().Unix())
+    filePath := filepath.Join(downloadDir, fileName)
+
+    out, err := os.Create(filePath)
+    if err != nil {
+        return fmt.Errorf("error creating output file: %w", err)
+    }
+    defer out.Close()
+
+    fmt.Printf("Downloading Instagram video\n")
+    fmt.Printf("Size: %.2f MB\n", float64(videoResp.ContentLength)/(1024*1024))
+
+    _, err = io.Copy(out, io.TeeReader(videoResp.Body, &ProgressWriter{Total: videoResp.ContentLength}))
+    if err != nil {
+        return fmt.Errorf("error saving video: %w", err)
+    }
+
+    fmt.Printf("\nVideo downloaded: %s\n", filePath)
+    return nil
 }
